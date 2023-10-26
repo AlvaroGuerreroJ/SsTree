5584146a74d6c465b57e3b0e86c67eced1cb3277
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2023-10-26 12:59:49 -0500

    style: Mostly style

--- src/SStree.cpp
+++ src/SStree.cpp
@@ -1,112 +1,13 @@
 #include "SStree.h"
 
 
-
-bool SsNode::test(bool isRoot) const {
-    size_t count = 0;
-    if (this->isLeaf()) {
-        const SsLeaf* leaf = dynamic_cast<const SsLeaf*>(this);
-        count = leaf->points.size();
-
-        // Verificar si los puntos están dentro del radio del nodo
-        for (const Point& point : leaf->points) {
-            if (distance(this->centroid, point) > this->radius) {
-                std::cout << "Point outside node radius detected." << std::endl;
-                return false;
-            }
-        }
-    } else {
-        const SsInnerNode* inner = dynamic_cast<const SsInnerNode*>(this);
-        count = inner->children.size();
-
-        // Verificar si los centroides de los hijos están dentro del radio del nodo padre
-        for (const SsNode* child : inner->children) {
-            if (distance(this->centroid, child->centroid) > this->radius) {
-                std::cout << "Child centroid outside parent radius detected." << std::endl;
-                return false;
-            }
-            // Verificar recursivamente cada hijo
-            if (!child->test(false)) {
-                return false;
-            }
-        }
-    }
-
-    // Comprobar la validez de la cantidad de hijos/puntos
-    if (!isRoot && (count < Settings::m || count > Settings::M)) {
-        std::cout << "Invalid number of children/points detected." << std::endl;
-        return false;
-    }
-
-    // Comprobar punteros de parentezco, salvo para el nodo raíz
-    if (!isRoot && !parent) {
-        std::cout << "Node without parent detected." << std::endl;
-        return false;
-    }
-
-    return true;
-}
-
-void SsTree::test() const {
-    bool result = root->test(true);
-
-    if (root->parent) {
-        std::cout << "Root node parent pointer is not null!" << std::endl;
-        result = false;
-    }
-
-    if (result) {
-        std::cout << "SS-Tree is valid!" << std::endl;
-    } else {
-        std::cout << "SS-Tree has issues!" << std::endl;
-    }
-}
-
-
-void SsNode::print(size_t indent = 0) const {
-    for (size_t i = 0; i < indent; ++i) {
-        std::cout << "  ";
-    }
-
-    // Imprime información del nodo.
-    std::cout << "Centroid: " << centroid << ", Radius: " << radius;
-    if (isLeaf()) {
-        const SsLeaf* leaf = dynamic_cast<const SsLeaf*>(this);
-        std::cout << ", Points: [ ";
-        for (const Point& p : leaf->points) {
-            std::cout << p << " ";
-        }
-        std::cout << "]";
-    } else {
-        std::cout << std::endl;
-        const SsInnerNode* inner = dynamic_cast<const SsInnerNode*>(this);
-        for (const SsNode* child : inner->children) {
-            child->print(indent + 1); 
-        }
-    }
-    std::cout << std::endl;
-}
-void SsTree::print() const {
-    if (root) {
-        root->print();
-    } else {
-        std::cout << "Empty tree." << std::endl;
-    }
-}
-
-
-
-
-
-
-
 void SsLeaf::saveToStream(std::ostream &out) const {
     bool isLeaf = true;
     out.write(reinterpret_cast<const char*>(&isLeaf), sizeof(isLeaf));
 
     // Guardar centroid y radius
-    out.write(reinterpret_cast<const char*>(&centroid), sizeof(centroid));
-    out.write(reinterpret_cast<const char*>(&radius), sizeof(radius));
+    out.write(reinterpret_cast<const char*>(&m_centroid), sizeof(m_centroid));
+    out.write(reinterpret_cast<const char*>(&m_radius), sizeof(m_radius));
 
     // Guardar los puntos
     size_t numPoints = points.size();
@@ -127,8 +28,8 @@ void SsLeaf::saveToStream(std::ostream &out) const {
 
 void SsLeaf::loadFromStream(std::istream &in) {
     // Leer centroid y radius
-    in.read(reinterpret_cast<char*>(&centroid), sizeof(centroid));
-    in.read(reinterpret_cast<char*>(&radius), sizeof(radius));
+    in.read(reinterpret_cast<char*>(&m_centroid), sizeof(m_centroid));
+    in.read(reinterpret_cast<char*>(&m_radius), sizeof(m_radius));
 
     // Leer puntos
     size_t numPoints;
@@ -172,8 +73,8 @@ void SsInnerNode::saveToStream(std::ostream &out) const {
     out.write(reinterpret_cast<const char*>(&isLeaf), sizeof(isLeaf));
 
     // Guardar centroid y radius
-    out.write(reinterpret_cast<const char*>(&centroid), sizeof(centroid));
-    out.write(reinterpret_cast<const char*>(&radius), sizeof(radius));
+    out.write(reinterpret_cast<const char*>(&m_centroid), sizeof(m_centroid));
+    out.write(reinterpret_cast<const char*>(&m_radius), sizeof(m_radius));
 
     // Guardar la cantidad de hijos para saber cuántos nodos leer después
     size_t numChildren = children.size();
@@ -187,8 +88,8 @@ void SsInnerNode::saveToStream(std::ostream &out) const {
 
 void SsInnerNode::loadFromStream(std::istream &in) {
     // Leer centroid y radius
-    in.read(reinterpret_cast<char*>(&centroid), sizeof(centroid));
-    in.read(reinterpret_cast<char*>(&radius), sizeof(radius));
+    in.read(reinterpret_cast<char*>(&m_centroid), sizeof(m_centroid));
+    in.read(reinterpret_cast<char*>(&m_radius), sizeof(m_radius));
 
     // Leer cantidad de hijos
     size_t numChildren;
--- src/SStree.h
+++ src/SStree.h
@@ -7,8 +7,8 @@
 #include <queue>
 #include <vector>
 
-#include "params.h"
 #include "Point.h"
+#include "params.h"
 #include "util.hpp"
 
 template<typename Data>
@@ -21,7 +21,8 @@ template<typename Data>
 class SsLeaf;
 
 template<typename Data>
-class SsNode {
+class SsNode
+{
 public:
     using leaf_t = SsLeaf<Data>;
     using innern_t = SsInnerNode<Data>;
@@ -31,9 +32,9 @@ public:
     SsNode* m_parent = nullptr;
 
 private:
-    NType varianceAlongDirection(const std::vector<Point>& centroids, size_t direction) const;
+    NType varianceAlongDirection(std::vector<Point> const& centroids, size_t direction) const;
     size_t minVarianceSplit(size_t coordinateIndex);
-    
+
 public:
     virtual ~SsNode() = default;
 
@@ -41,7 +42,9 @@ public:
     virtual std::vector<Point> getEntriesCentroids() const = 0;
     virtual void sortEntriesByCoordinate(size_t coordinateIndex) = 0;
     virtual std::pair<SsNode*, SsNode*> split() = 0;
-    virtual bool intersectsPoint(const Point& point) const {
+
+    virtual bool intersectsPoint(Point const& point) const
+    {
         return distance(this->m_centroid, point) <= this->m_radius;
     }
 
@@ -49,87 +52,109 @@ public:
     size_t directionOfMaxVariance() const;
     size_t findSplitIndex();
 
-    virtual SsNode* insert(const Point& point) = 0;
+    virtual SsNode* insert(Point const& point) = 0;
 
-    bool test(bool isRoot = false) const {
-      size_t count = 0;
-      if (this->isLeaf()) {
-        const leaf_t *leaf = dynamic_cast<const leaf_t *>(this);
-        count = leaf->m_points.size();
+    bool test(bool isRoot = false) const
+    {
+        size_t count = 0;
+        if (this->isLeaf())
+        {
+            leaf_t const* leaf = dynamic_cast<leaf_t const*>(this);
+            count = leaf->m_points.size();
 
-        // Verificar si los puntos están dentro del radio del nodo
-        for (const Point &point : leaf->m_points) {
-          if (distance(this->m_centroid, point) > this->m_radius) {
-            std::cout << "Point outside node radius detected." << std::endl;
-            return false;
-          }
+            // Verificar si los puntos están dentro del radio del nodo
+            for (Point const& point : leaf->m_points)
+            {
+                if (distance(this->m_centroid, point) > this->m_radius)
+                {
+                    std::cout << "Point outside node radius detected." << std::endl;
+                    return false;
+                }
+            }
         }
-      } else {
-        const innern_t *inner = dynamic_cast<const innern_t *>(this);
-        count = inner->m_children.size();
+        else
+        {
+            innern_t const* inner = dynamic_cast<innern_t const*>(this);
+            count = inner->m_children.size();
 
-        // Verificar si los centroides de los hijos están dentro del radio del
-        // nodo padre
-        for (const SsNode *child : inner->m_children) {
-          if (distance(this->m_centroid, child->m_centroid) > this->m_radius) {
-            std::cout << "Child centroid outside parent radius detected."
-                      << std::endl;
-            return false;
-          }
-          // Verificar recursivamente cada hijo
-          if (!child->test(false)) {
-            return false;
-          }
+            // Verificar si los centroides de los hijos están dentro del radio del
+            // nodo padre
+            for (SsNode const* child : inner->m_children)
+            {
+                if (distance(this->m_centroid, child->m_centroid) > this->m_radius)
+                {
+                    std::cout << "Child centroid outside parent radius detected." << std::endl;
+                    return false;
+                }
+                // Verificar recursivamente cada hijo
+                if (!child->test(false))
+                {
+                    return false;
+                }
+            }
         }
-      }
 
-      // Comprobar la validez de la cantidad de hijos/puntos
-      if (!isRoot && (count < Settings::m || count > Settings::M)) {
-        std::cout << "Invalid number of children/points detected." << std::endl;
-        return false;
-      }
+        // Comprobar la validez de la cantidad de hijos/puntos
+        if (!isRoot && (count < Settings::m || count > Settings::M))
+        {
+            std::cout << "Invalid number of children/points detected." << std::endl;
+            return false;
+        }
 
-      // Comprobar punteros de parentezco, salvo para el nodo raíz
-      if (!isRoot && !m_parent) {
-        std::cout << "Node without parent detected." << std::endl;
-        return false;
-      }
+        // Comprobar punteros de parentezco, salvo para el nodo raíz
+        if (!isRoot && !m_parent)
+        {
+            std::cout << "Node without parent detected." << std::endl;
+            return false;
+        }
 
-      return true;
+        return true;
     }
 
-    void print(size_t indent = 0) const {
-      for (size_t i = 0; i < indent; ++i) {
-        std::cout << "  ";
-      }
-
-      // Imprime información del nodo.
-      std::cout << "Centroid: " << m_centroid << ", Radius: " << m_radius;
-      if (isLeaf()) {
-        const leaf_t *leaf = dynamic_cast<const leaf_t *>(this);
-        std::cout << ", Points: [ ";
-        for (const Point &p : leaf->m_points) {
-          std::cout << p << " ";
+    void print(size_t indent = 0) const
+    {
+        for (size_t i = 0; i < indent; ++i)
+        {
+            std::cout << "  ";
         }
-        std::cout << "]";
-      } else {
+
+        // Imprime información del nodo.
+        std::cout << "Centroid: " << m_centroid << ", Radius: " << m_radius;
+        if (isLeaf())
+        {
+            leaf_t const* leaf = dynamic_cast<leaf_t const*>(this);
+            std::cout << ", Points: [ ";
+            for (Point const& p : leaf->m_points)
+            {
+                std::cout << p << " ";
+            }
+            std::cout << "]";
+        }
+        else
+        {
+            std::cout << std::endl;
+            innern_t const* inner = dynamic_cast<innern_t const*>(this);
+            for (SsNode const* child : inner->m_children)
+            {
+                child->print(indent + 1);
+            }
+        }
         std::cout << std::endl;
-        const innern_t *inner = dynamic_cast<const innern_t *>(this);
-        for (const SsNode *child : inner->m_children) {
-          child->print(indent + 1);
-        }
-      }
-      std::cout << std::endl;
     }
 
-    virtual void FNDFTrav(const Point& q, size_t k, std::priority_queue<Pair, std::vector<Pair>, Comparator>& L, NType& Dk) const = 0;
+    virtual void FNDFTrav(
+        Point const& q,
+        size_t k,
+        std::priority_queue<Pair, std::vector<Pair>, Comparator>& L,
+        NType& Dk) const = 0;
 
-    virtual void saveToStream(std::ostream &out) const = 0;
-    virtual void loadFromStream(std::istream &in) = 0;
+    virtual void saveToStream(std::ostream& out) const = 0;
+    virtual void loadFromStream(std::istream& in) = 0;
 };
 
 template<typename Data>
-class SsInnerNode : public SsNode<Data> {
+class SsInnerNode : public SsNode<Data>
+{
 public:
     using node_t = SsNode<Data>;
 
@@ -142,20 +167,30 @@ private:
 public:
     std::pair<node_t*, node_t*> split() override;
 
-    node_t* findClosestChild(const Point& target) const;
-    bool isLeaf() const override { return false; }
+    node_t* findClosestChild(Point const& target) const;
+
+    bool isLeaf() const override
+    {
+        return false;
+    }
+
     void updateBoundingEnvelope() override;
 
-    node_t* insert(const Point& point) override;
+    node_t* insert(Point const& point) override;
 
-    void FNDFTrav(const Point& q, size_t k, std::priority_queue<Pair, std::vector<Pair>, Comparator>& L, NType& Dk) const override;
+    void FNDFTrav(
+        Point const& q,
+        size_t k,
+        std::priority_queue<Pair, std::vector<Pair>, Comparator>& L,
+        NType& Dk) const override;
 
-    virtual void saveToStream(std::ostream &out) const override;
-    virtual void loadFromStream(std::istream &in) override;
+    virtual void saveToStream(std::ostream& out) const override;
+    virtual void loadFromStream(std::istream& in) override;
 };
 
 template<typename Data>
-class SsLeaf : public SsNode<Data> {
+class SsLeaf : public SsNode<Data>
+{
 public:
     using node_t = SsNode<Data>;
 
@@ -169,59 +204,82 @@ private:
 public:
     std::pair<node_t*, node_t*> split() override;
 
-    bool isLeaf() const override { return true; }
+    bool isLeaf() const override
+    {
+        return true;
+    }
+
     void updateBoundingEnvelope() override;
 
-    node_t* insert(const Point& point) override;
+    node_t* insert(Point const& point) override;
 
-    void FNDFTrav(const Point& q, size_t k, std::priority_queue<Pair, std::vector<Pair>, Comparator>& L, NType& Dk) const override;
+    void FNDFTrav(
+        Point const& q,
+        size_t k,
+        std::priority_queue<Pair, std::vector<Pair>, Comparator>& L,
+        NType& Dk) const override;
 
-    virtual void saveToStream(std::ostream &out) const override;
-    virtual void loadFromStream(std::istream &in) override;
+    virtual void saveToStream(std::ostream& out) const override;
+    virtual void loadFromStream(std::istream& in) override;
 };
 
 template<typename Data>
-class SsTree {
+class SsTree
+{
 public:
     using node_t = SsNode<Data>;
 
     node_t* root;
-    node_t* search(node_t* node, const Point& target);
-    node_t* searchParentLeaf(node_t* node, const Point& target);
+    node_t* search(node_t* node, Point const& target);
+    node_t* searchParentLeaf(node_t* node, Point const& target);
 
 public:
-    SsTree() : root(nullptr) {}
-    ~SsTree() {
+    SsTree()
+        : root(nullptr)
+    {
+    }
+
+    ~SsTree()
+    {
         delete root;
     }
-    
-    void insert(const Point& point, const std::string& data);
-    void build (const std::vector<Point>& points);
-    std::vector<Point> kNNQuery(const Point& center, size_t k) const;
 
-    void print() const {
-      if (root) {
-        root->print();
-      } else {
-        std::cout << "Empty tree." << std::endl;
-      }
+    void insert(Point const& point, std::string const& data);
+    void build(std::vector<Point> const& points);
+    std::vector<Point> kNNQuery(Point const& center, size_t k) const;
+
+    void print() const
+    {
+        if (root)
+        {
+            root->print();
+        }
+        else
+        {
+            std::cout << "Empty tree." << std::endl;
+        }
     }
 
-    void test() const {
-      bool result = root->test(true);
+    void test() const
+    {
+        bool result = root->test(true);
 
-      if (root->m_parent) {
-        std::cout << "Root node parent pointer is not null!" << std::endl;
-        result = false;
-      }
+        if (root->m_parent)
+        {
+            std::cout << "Root node parent pointer is not null!" << std::endl;
+            result = false;
+        }
 
-      if (result) {
-        std::cout << "SS-Tree is valid!" << std::endl;
-      } else {
-        std::cout << "SS-Tree has issues!" << std::endl;
-      }
+        if (result)
+        {
+            std::cout << "SS-Tree is valid!" << std::endl;
+        }
+        else
+        {
+            std::cout << "SS-Tree has issues!" << std::endl;
+        }
     }
 
-    void saveToFile(const std::string &filename) const;
-    void loadFromFile(const std::string &filename);
+    void saveToFile(std::string const& filename) const;
+    void loadFromFile(std::string const& filename);
 };
